f72af03be91d77fd0ba437af820a31c59e8424d3
Pangsss <afiq010507@gmail.com>
2023-12-23 23:59:07 +0800

    Update login and signup

--- Med_Diagnotion_app/urls.py
+++ Med_Diagnotion_app/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup'),
+    path('dashboard', views.dashboard_view, name='dashboard'),
 ]
--- Med_Diagnotion_app/views.py
+++ Med_Diagnotion_app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.utils import timezone
+from .models import CustomUser
 
 # Create your views here.
 
@@ -31,3 +33,38 @@ def login_view(request):
             return redirect('login')
 
     return render(request, 'login.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirmpassword')
+        gender = request.POST.get('gender')
+
+        print(email, first_name, last_name, password, confirm_password, gender)
+
+        # Check if passwords match
+        if password != confirm_password:
+            # Handle password mismatch error
+            messages.error(request, 'Password do not match!')
+            return redirect('signup')
+
+        # Create user
+        user = CustomUser.objects.create_user(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            gender=gender,
+            date_joined=timezone.now()
+        )
+
+        return redirect('login')
+    return render(request, 'signup.html')
+
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
